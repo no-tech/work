6aa13e7f58b87a378415e56890406fe52ed0f480
Will <wjakesta@gmail.com>
2015-04-22 13:17:47 -0700

    added missing end

--- calculator.rb
+++ calculator.rb
@@ -3,18 +3,16 @@ class Calculator
    # adds a to b
     a + b
   end
-
   def subtract(a, b)
    # subtracts a from b
     a - b
   end
-
   def divide(a, b)
    # divides a from b
     a / b
-
-   def multiply(a, b)
-   	# multiplys a with b
-   	a * b
+  end
+  def multiply(a, b)
+   # multiplys a with b
+   a * b
   end
 end
